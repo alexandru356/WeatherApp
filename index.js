5e4272d69b2897c8dde3b724697233bdfa064f27
Alex Ciuca <alexandru356.c@gmail.com>
2024-08-02 22:01:20 -0700

    added some js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello pookie caca');
+const example = document.getElementById("btn1").addEventListener("click", expand);
+
+function expand(){
+    //insert a condition to see if the city is valid, if not show an error
+    let input = document.getElementById("search");
+    if(input.value.trim() === ""){
+        return alert("Please enter a city name !");
+    }
+    return document.getElementById("container").style.height = "400px",
+    document.getElementById("container").style.transition = "height 1s"; 
+}
